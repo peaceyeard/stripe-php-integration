7dfabf7adc079409190f29e8e6403816d5d5c1aa
Mazumder Nazmul <mazumdernazmul00@gmail.com>
2020-10-22 13:40:23 +0600

    Gateway library and required files added

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php 
+    define("STRIPE_SECRET_KEY", "sk_test_51HeaeEAWppqhhwkG8WMSgbjhfRBwMYYqXgMTxdiXbzYiqci3zgUs2VS7D4nzDkY4YnyakleE6oKsvfRn1rA0Hm6i00HpPD6FZw");
+    define("STRIPE_PUBLISHABLE_KEY", "pk_test_51HeaeEAWppqhhwkGsJODrndaQXwLkZIdmWhGhKrAXmReGSyLb9Sj1yrsaMeK6JPZLWrvUD7U0pew796r6jyIj6LT00l69FlxcH");
+?>
